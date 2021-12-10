77ff8a1ab62f234dd155828ead2d825314c2f53e
3PC112-72-W10N\vt167 <vt167098@hotmail.com>
2021-12-10 10:50:23 +0800

    增加router範例

--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router';
 import Home from './components/Home.vue';
+import Form120 from './components/Form120.vue';
 
 const routes = [{
     path: '/',
     name: 'home',
     component: Home,
+}, {
+    path: '/cntm120',
+    name: 'cntm120',
+    component: Form120,
 }, ];
 
 const router = createRouter({
